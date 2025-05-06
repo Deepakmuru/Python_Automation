c46b1b7d6e4d9c0e3d05f7186853b99275f0a8ba
Deepakmuru <deepakmurugesan01@gmail.com>
2025-05-06 16:09:37 +0530

    LocatorsExtension

---
+++ LocatorsExtension.py
@@ -0,0 +1,19 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://www.demoblaze.com/")
+driver.maximize_window()
+driver.find_element(By.ID,"cartur").click()
+driver.find_element(By.CSS_SELECTOR,"button.btn.btn-success").click()
+time.sleep(3)
+driver.find_element(By.ID,"name").send_keys("Deepak Testing")
+driver.find_element(By.ID,"country").send_keys("India")
+driver.find_element(By.ID,"city").send_keys("Bangalore")
+driver.find_element(By.ID,"card").send_keys("564555645435")
+driver.find_element(By.ID,"month").send_keys("May")
+driver.find_element(By.ID,"year").send_keys("2025")
+driver.find_element(By.XPATH,"//button[@onclick='purchaseOrder()']").click()
+time.sleep(3)
